f076627463a7ee6ecf59afbb85c1d7a305bb2b25
nhamilton1 <nickh3592@gmail.com>
2021-09-28 12:40:40 -0400

    did get and get posts by id

--- api/server.js
+++ api/server.js
@@ -1,2 +1,21 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express();
+
+const postsRouter = require('./posts/posts-router')
+
+server.use(express.json()); // ********teaches express to read req.body as JSON*****
+
+server.use('/api/posts', postsRouter)
+
+server.get('*', (req, res) => {
+    res.send(`
+      <h2>Lambda Shelter API</h>
+      <p>Welcome to the Lambda Shelter API</p>
+    `);
+  });
+  
+  module.exports = server;
+  
